52adf195914665c7328449d15f8fb858071f352b
Zamtas Int <zamtasint2@gmail.com>
2024-09-21 16:00:31 +0500

    UPDATED

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
